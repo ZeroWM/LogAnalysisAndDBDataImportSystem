266198600f29c2c1300f1120c6599442cadde3dc
ZeroWM <wm4968@gmail.com>
2021-09-24 18:57:07 +0800

    feat: add dismantled with hatchet

--- src/main/java/service/AnalysisDayZTask.java
+++ src/main/java/service/AnalysisDayZTask.java
@@ -9,37 +9,42 @@ import jdk.dynalink.beans.StaticClass;
 /**
  * DayZ服务器日志分析定时任务
  *
- * 作者：豆芽 Dora
- * 版权：DayZ金三角服务器所有，仅可用于非盈利服务器
+ * <p>作者：豆芽 Dora 版权：DayZ金三角服务器所有，仅可用于非盈利服务器
  */
 public class AnalysisDayZTask {
 
-  /**
-   * 1. 读取文件, 设计实体 2. 数据代码入库 3. 页面展示数据，分页查询，并支持根据关键字搜索
-   */
-
+  /** 1. 读取文件, 设计实体 2. 数据代码入库 3. 页面展示数据，分页查询，并支持根据关键字搜索 */
   public static void main(String[] args) {
     File file = new File("/Users/wm/Downloads/ZhengShiFu_2021_09_24_055229001.ADM");
 
-//    File file = new File("/Users/wm/Downloads/loli-24.rtf");
+    //    File file = new File("/Users/wm/Downloads/loli-24.rtf");
     String result = txt2String(file);
     System.out.printf(result);
   }
 
-  //placed Car Tent 放置汽车帐篷
-  //Dismantled Lower Frame from Fence with Hatchet 采用小手斧拆墙
+  // placed Car Tent 放置汽车帐篷
+  // Dismantled Lower Frame from Fence with Hatchet 采用小手斧拆墙
   public static String txt2String(File file) {
     StringBuilder result = new StringBuilder();
     try {
       BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
       String str = null;
       while ((str = bufferedReader.readLine()) != null) {
-        if (str.contains("Tent")) {
-          result.append(System.lineSeparator() + str.replace("Tent","放置帐篷"));
+        if (str.contains("placed Wooden Crate")) {
+          result.append(System.lineSeparator() + str.replace("placed Wooden Crate", "放置原木"));
         }
-        if(str.contains("Dismantled Lower Frame from Fence with Hatchet")){
-          result.append(System.lineSeparator() + str.replace("Dismantled Lower Frame from Fence with Hatchet","通过小手斧拆墙"));
-
+        if (str.contains("Tent")) {
+          result.append(System.lineSeparator() + str.replace("Tent", "放置帐篷"));
+        }
+        if (str.contains("Dismantled Lower Frame from Fence with Hatchet")) {
+          result.append(
+              System.lineSeparator()
+                  + str.replace("Dismantled Lower Frame from Fence with Hatchet", "通过小手斧拆下面墙"));
+        }
+        if (str.contains("Dismantled Upper Frame from Fence with Hatchet")) {
+          result.append(
+              System.lineSeparator()
+                  + str.replace("Dismantled Upper Frame from Fence with Hatchet", "通过小手斧拆上面墙"));
         }
       }
       bufferedReader.close();
@@ -49,6 +54,4 @@ public class AnalysisDayZTask {
 
     return result.toString();
   }
-
-
 }
